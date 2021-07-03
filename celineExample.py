2bddd2a658388c01134e6cd2610152932897a2b2
Jun Ping <junping@bambu.co>
2021-07-03 20:21:46 +0800

    Celines first commit.

---
+++ celineExample.py
@@ -0,0 +1,10 @@
+print("Hello World")
+print("I love Jun Ping")
+print("Jun Ping is a smarty pants")
+
+junPing = ""
+
+if junPing == 1:
+    print("Jun Ping is smarty")
+else:
+    print("Jun Ping is dummy")
